64ca40aaa039131f74283ad10d9451663481f98e
Gador Cruz <gadorcruz@hotmail.com>
2023-01-24 18:33:56 +0000

    seeding

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  RATING = (0..5).to_a
+  validates :content, presence: true
+  validates :rating, inclusion: { in: RATING }, presence: true, numericality: { only_integer: true }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+i = 0
+5.times do
+  i += 1
+  puts "Creating restaurant #{i}"
+  Restaurant.create(
+    name: Faker::Creature::Dog.name,
+    address: Faker::Address.street_name,
+    phone_number: ,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  Review.create(
+    rating: Rand(0..5),
+    content:
+  )
+end
